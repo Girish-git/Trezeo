df3a6971743609300b177ac45e31743f8afec46a
Girish-git <girish.k9@gmail.com>
2018-08-02 16:33:47 +0530

    Add files via upload

---
+++ Trezeo_Assignment/CommonAncestor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonAncestor
+{
+    public class TreeNode
+    {
+        // The value held by the node
+        public int Value { get; }
+        // A link to the parent node in the tree
+        public TreeNode Parent { get; }
+        // A link to the left child node in the tree
+        public TreeNode Left { get; }
+        // A link to the right child node in the tree
+        public TreeNode Right { get; }
+    }
+
+    class BST
+    {
+        // Create Tree Methods
+
+        TreeNode root;
+
+        //Search Common Ancestor
+        public static TreeNode LowestCommonAncestor(TreeNode node, TreeNode n1, TreeNode n2)
+        {
+            if (node == null)
+                return null;
+
+            if (node.Value > n1.Value && node.Value > n2.Value)
+                return LowestCommonAncestor(node.Left, n1, n2);
+
+            if (node.Value < n1.Value && node.Value < n2.Value)
+                return LowestCommonAncestor(node.Right, n1, n2);
+            return node;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize tree 
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Trezeo_Assignment/Trezeo_Assignment/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trezeo_Assignment
+{
+    public class Lifo<T>
+    {
+        int capacity;
+        T[] stack;
+        int top;
+
+        public Lifo(int size)
+        {
+            capacity = size;
+            stack = new T[capacity];
+        }
+
+        public void Push(T item)
+        {
+            if (top != capacity - 1)
+            {
+                top = top + 1;
+                stack[top] = item;
+            }
+        }
+        public T Pop()
+        {
+            T RemovedElement;
+            T temp = default(T);
+            if (!(top <= 0))
+            {
+                RemovedElement = stack[top];
+                top = top - 1;
+                return RemovedElement;
+            }
+            return temp;
+        }
+
+        public int Count()
+        {
+            return top;
+        }
+    }
+
+    public class Fifo<T>
+    {
+        private Lifo<T> _lifo1;
+        private Lifo<T> _lifo2;
+        public Fifo(int size)
+        {
+            _lifo1 = new Lifo<T>(size);
+            _lifo2 = new Lifo<T>(size);
+        }
+
+        public void Enqueue(T item)
+        {
+            _lifo1.Push(item);
+        }
+
+        public T Dequeue()
+        {
+            if (_lifo1.Count() > 0)
+            {
+                _lifo2.Push(_lifo1.Pop());
+            }
+            return _lifo2.Pop();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fifo<int> queue = new Fifo<int>(100);
+            queue.Enqueue(100);
+            queue.Enqueue(200);
+            Console.WriteLine(queue.Dequeue());
+            queue.Enqueue(300);
+            queue.Enqueue(400);
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.ReadLine();
+        }
+    }
+}
